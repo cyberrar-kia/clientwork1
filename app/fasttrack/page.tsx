6e7b0d61ef9c64e3ea86e0952fa2603b569f0429
Joshua <joshua@scaltron.com>
2026-06-12 17:09:14 +0000

    Fix build error: FastTrackPage function name was broken by sed replacement

--- app/fasttrack/page.tsx
+++ app/fasttrack/page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Client WorkPage() {
+export default function ClientWorkPage() {
   const [whyOpen, setWhyOpen] = useState(false);
   const [whoOpen, setWhoOpen] = useState(false);
   const [diffOpen, setDiffOpen] = useState(false);
